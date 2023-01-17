50f4dcdcd22a7fc0a592c15e90cfc9d7cb0bfa8c
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-17 14:06:36 -0300

    ...

---
+++ 01- Bin2Dec/jss.js
@@ -0,0 +1,17 @@
+let entrada = document.getElementById(`entrada`);
+
+let submit = document.querySelector(`submit`);
+
+// Seleciona o formulário
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Recupera o valor do input
+  const value = input.value;
+
+
+
+
+//let resposta = document.getElementById(`resposta`);
+
