d13ac53305760711177dcf086993ce12dde8a4e3
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-18 13:21:35 -0300

    html e CSS OK

---
+++ 02-Border-radius Previewer/jss.js
@@ -0,0 +1,19 @@
+
+
+
+const esquerdaS = document.querySelector('.esquerda-S');
+
+const borda = document.querySelector('.borda');
+
+
+
+const pontaEsquerda = esquerdaS.value;
+
+p
+
+function test(){
+
+return document.borda.style.borderradius = ;
+
+
+}
