10974acb09114052e668eb478669f556554d991c
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-19 20:05:32 -0300

    .....

--- 03- Calculator/jss.js
+++ 03- Calculator/jss.js
@@ -1,31 +1,39 @@
-function clicou (){
-let numeros = document.querySelectorAll(`.numeros`).values
+
+const numeros1 = document.getElementById(`numeros1`).innerHTML;
+const numeros2 = document.getElementById(`numeros2`).innerHTML;
+const numeros3 = document.getElementById(`numeros3`).innerHTML;
+const numeros4 = document.getElementById(`numeros4`).innerHTML;
+const numeros5 = document.getElementById(`numeros5`).innerHTML;
+const numeros6 = document.getElementById(`numeros6`).innerHTML;
+const numeros7 = document.getElementById(`numeros7`).innerHTML;
+const numeros8 = document.getElementById(`numeros8`).innerHTML;
+const numeros9 = document.getElementById(`numeros9`).innerHTML;
+
+parseInt(numeros1)
+console.log(numeros1)
 
 
-    
-console.log(numeros)
-
-console.log(parseInt(numeros))
 
 
+
+function clicou(){
+console.log(numeros1)
+console.log(numeros2)
+console.log(numeros3)
+console.log(numeros4)
+console.log(numeros5)
+console.log(numeros6)
+console.log(numeros7)
+console.log(numeros8)
+console.log(numeros9)
+
+
+return console.log(parseInt(numeros1))
 
 
 }
 
 
-/*
-let operadorAC = document.getElementById(`numeros`).innerHTML;
-
-function filtro(antes){
-   return parseInt(antes)
-}
-
-
-
-
-return console.log(filtro(operadorAC))
-
-*/
 
 
 
