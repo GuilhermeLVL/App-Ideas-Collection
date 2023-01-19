c329b80903755a1d902bc6a4681758da18f1bb7a
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-19 19:05:26 -0300

    ...

---
+++ 03- Calculator/jss.js
@@ -0,0 +1,46 @@
+function clicou (){
+let numeros = document.querySelectorAll(`.numeros`).values
+
+
+    
+console.log(numeros)
+
+console.log(parseInt(numeros))
+
+
+
+
+}
+
+
+/*
+let operadorAC = document.getElementById(`numeros`).innerHTML;
+
+function filtro(antes){
+   return parseInt(antes)
+}
+
+
+
+
+return console.log(filtro(operadorAC))
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let valor =  parseInt(  operadorAC.innerHTML
+)    
+return console.log(valor) (operadorAC.innerHTML)*/
