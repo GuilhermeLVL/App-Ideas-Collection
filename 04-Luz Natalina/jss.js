824d3071bc1e8086eeaf875b14ba1a445c48aecb
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-25 20:20:55 -0300

    js feito

--- 04-Luz Natalina/jss.js
+++ 04-Luz Natalina/jss.js
@@ -1,6 +1,22 @@
-let luz01 = document.querySelectorAll(`.luz01`);
+let luz01 = document.querySelectorAll('.luz01')
+let luz02 = document.querySelectorAll('.luz02')
+let luz03 = document.querySelectorAll('.luz03')
+let luz04 = document.querySelectorAll('.luz04')
 
-function luzOff(){
-luz01.style.animation = "none";
+function luzOffs(){
+for(let i=0; i<=2; i++){
+    luz01[i].style.animation = "none";
+    luz02[i].style.animation = "none";
+    luz03[i].style.animation = "none";
+    luz04[i].style.animation = "none";
+}
+}
 
+function luzOn() {
+    for(let i=0; i<=2; i++){
+        luz01[i].style.animation =  "luzVermelha 1s infinite 1s";
+        luz02[i].style.animation =  "luzAmarela 1.5s infinite 1.5s";
+        luz03[i].style.animation =  "luzVerde 1s infinite 1s";
+        luz04[i].style.animation =  "luzAzul 1.5s infinite 1.5s";
+    }
 }
