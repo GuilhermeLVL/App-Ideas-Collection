13baef5be69930c3515e02c590d56294210508a2
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-30 19:03:51 -0300

    Feito

--- 06-ColorCycle/jss.js
+++ 06-ColorCycle/jss.js
@@ -9,14 +9,16 @@ let green =  document.getElementById(`green`);
 function retunColor(){
     cores.style.backgroundColor = "rgb(" + red.value + "," + green.value + "," + blue.value + ")";
     valor.innerHTML = `#${red.value},${green.value},${blue.value}`;
+
 }
 
 
 function numAleatorio(){
- red = Math.round(Math.random() * 255);
- green = Math.round(Math.random() * 255);
- blue = Math.round(Math.random() * 255);
 
- cores.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
- valor.innerHTML = `#${red},${green},${blue}`;
+ let red2 = Math.round(Math.random() * 255);
+ let green2 = Math.round(Math.random() * 255);
+ let blue2 = Math.round(Math.random() * 255);
+
+ cores.style.backgroundColor = "rgb(" + red2 + "," + green2 + "," + blue2 + ")";
+ valor.innerHTML = `#${red2},${green2},${blue2}`;
 }
