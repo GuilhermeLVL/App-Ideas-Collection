0c799a443326598d580aaf810034d62b753724bb
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-31 18:38:15 -0300

    ..

--- 07-Countdown Timer/jss.js
+++ 07-Countdown Timer/jss.js
@@ -3,10 +3,22 @@ let data = document.querySelector(`.date`)
 
 //Passagem de dados
 let dias = document.querySelector(`#dias`)
-let horas = document.querySelector(`#horas`)
+let horas = document.querySelector(`#horas`) 
 let minutos = document.querySelector(`#minutos`)
 let segundos = document.querySelector(`#segundos`)
 
+
+
+/*Dados atuais */
+let dateAt = new Date();
+
+
+
 function coletaData(){
-    console.log(data.value)
+
+    console.log(dateAt.getDate())
+    console.log(dateAt.getHours())
+    console.log(dateAt.getMinutes())
+    console.log(dateAt.getSeconds())
+
 }
