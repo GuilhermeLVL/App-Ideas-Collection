ab1f74a6bee1c2e7f02f54bfdb22ac89d14fa089
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-01-31 18:11:10 -0300

    Coletando dados com o input

---
+++ 07-Countdown Timer/jss.js
@@ -0,0 +1,12 @@
+//Entrada de dados
+let data = document.querySelector(`.date`)
+
+//Passagem de dados
+let dias = document.querySelector(`#dias`)
+let horas = document.querySelector(`#horas`)
+let minutos = document.querySelector(`#minutos`)
+let segundos = document.querySelector(`#segundos`)
+
+function coletaData(){
+    console.log(data.value)
+}
