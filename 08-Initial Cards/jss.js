a96afcfb8fb8fee4dd608889806e221d7242722b
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-06 15:00:31 -0300

    ...

--- 08-Initial Cards/jss.js
+++ 08-Initial Cards/jss.js
@@ -1,11 +1,24 @@
+//Pegando elemento
 const panels = document.querySelectorAll(`.panel`);
- 
-panels.forEach((panel) => {
+
+
+// forEach adiciona a funcao para todos os elementos do array(panels)
+
+
+/*Primeiro remove depois adiciona a funcao `active` */
+panels.forEach( panel => {
+    /* Ao clicar aciona a funcao "removeActiveClasses()"
+    Adiciona ao elemento clicado a classe `active`. */
     panel.addEventListener(`click`,() => {
         removeActiveClasses()
        panel.classList.add(`active`)
     })
 })
+/*Funcao remove a classe `active` */
 function removeActiveClasses(){
-    
+    panels.forEach( panel => {
+        panel.classList.remove(`active`)
+    })
 }
+
+
