863a1b2d68495fbb104c407453ed25795b12131b
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-22 23:29:29 -0300

    Update jss.js

--- 15-From Wave Animation/jss.js
+++ 15-From Wave Animation/jss.js
@@ -1,7 +1,7 @@
 const labels = document.querySelectorAll(`.form-controllabel`)
 
 labels.forEach(label => {
-    label.innerHTML = label.inneText
+    label.innerHTML = label.inne
     .split('')
     .map((letter, idx) => `<span>${letter}</span>`)
     .join('')
