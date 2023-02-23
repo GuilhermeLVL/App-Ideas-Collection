5bfa2df36da50bf0c3ba93c7d180a7bff8e2586d
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-23 13:59:57 -0300

    Update jss.js

--- 15-From Wave Animation/jss.js
+++ 15-From Wave Animation/jss.js
@@ -1,8 +1,8 @@
-const labels = document.querySelectorAll(`.form-control label`)
+const labels = document.querySelectorAll(`.form-controllabel`)
 
 labels.forEach(label => {
     label.innerHTML = label.inne
     .split('')
-    .map((letter, idx) => `<span>${letter}</span>`)
+    .map((letter, idx) => `<span style="transition-delay${idx * 50}ms">${letter}</span>`)
     .join('')
 })
