80606f2e552f99caa88e60a8dd9446804142928d
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-28 19:39:52 -0300

    1

---
+++ 18-key boxes/jss.js
@@ -0,0 +1,21 @@
+const inserts = document.getElementById('insert')
+
+window.addEventListener('keydowm', (event) => {
+inserts.innerHTML = `
+
+<div class="key">
+            ${event.key === ' ' ? 'Space' : e.key}
+            <small>event.key</small>
+        </div>
+
+        <div class="key">
+            ${event.keyCode}
+            <small>event.keyCode</small>
+        </div>
+
+        <div class="key">
+            ${event.code}
+            <small>event.code</small>
+        </div>
+`
+})
