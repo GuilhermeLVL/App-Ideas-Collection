b175353f8ae6c60d713846c6524c21d28d08c3e1
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-07 16:05:40 -0300

    Feito

--- 22 - Counters/jss.js
+++ 22 - Counters/jss.js
@@ -10,6 +10,9 @@ const updateCounter = () =>{
     const increment = target / 200
     if(c < target){
 counter.innerText =`${ Math.ceil( c + increment)}`
+setTimeout(updateCounter,1)
+    } else{
+        counter.innerText = target
     }
 }
 
