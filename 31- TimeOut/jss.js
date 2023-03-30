d37b56db5991bfa28f0efd3805ac88587e58c282
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-30 16:14:53 -0300

    a

--- 31- TimeOut/jss.js
+++ 31- TimeOut/jss.js
@@ -1,12 +1,15 @@
 const header = document.getElementById('header')
 const title = document.getElementById('title')
 const except = document.getElementById('except')
-const profile_img = document.getElementById('profile')
-const name = document.getElementById('name')
+const profile_img = document.getElementById('profile_img')
+const namez = document.getElementById('name')
 const date = document.getElementById('date')
 
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+
+setTimeout(getData, 2500)
 
 function getData(){
     
@@ -14,6 +17,8 @@ function getData(){
    title.innerHTML = 'Lorem ipsum dolor sit amet'
    except.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quae!'
    profile_img.innerHTML = ' <img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
-   name.innerHTML = 'John Doe'
+   namez.innerHTML = 'John Doe'
    date.innerHTML = 'oct 08, 2020'
+
+   animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
 }
