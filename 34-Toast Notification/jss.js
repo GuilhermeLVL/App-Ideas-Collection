3599759fb7b5fdb34ff1fc07b861d4b9aac81ba0
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-05 13:03:22 -0300

    Update jss.js

---
+++ 34-Toast Notification/jss.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('button')
+const toast = document.getElementById('toast')
+
+const messages = [
+'Ola 1',
+'Ola 2',
+'Ola 3',
+'Ola 4',
+]
+button.addEventListener('click', () => createNotification())
+
+function createNotification(){
+const notif = document.createElement('div')
+}
