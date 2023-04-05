5a2b3db452a961da672aa63b4f2b0ca69f0ec368
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-05 13:12:38 -0300

    Update jss.js

--- 34-Toast Notification/jss.js
+++ 34-Toast Notification/jss.js
@@ -11,4 +11,12 @@ button.addEventListener('click', () => createNotification())
 
 function createNotification(){
 const notif = document.createElement('div')
+notif.classList.add('toast')
+
+notif.innerText = getRandomMessage()
+
+}
+
+function getRandomMessage(){
+    return messages[Math.floor(Math.random() * messages.length)]    
 }
