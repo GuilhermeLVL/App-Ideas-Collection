dab4c1c24fae4a386dae0c908681409196fa3471
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-10 17:03:16 -0300

    a

--- 35 - GitHub Profile/jss.js
+++ 35 - GitHub Profile/jss.js
@@ -1,6 +1,6 @@
 const APIURL = 'https://api.github.com/users/'
 
-
+const main= document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -17,6 +17,40 @@ const { data } = await axios(APIURL + username)
     
 
 }
+
+function createUserCard{
+
+    const cardHTML = ` 
+    <div class="card">
+    <div>
+
+        <img src="https://randomuser.me/api/portraits/men/30.jpg" alt="" class="avatar">
+
+    </div>
+
+    <div class="user-info">
+        <h2>Johndoe</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, blanditiis?</p>
+
+        <ul>
+            <li>300 <strong>Followers</strong></li>
+            <li>100 <strong>Following</strong></li>
+            <li>30 <strong>Repos</strong></li>
+        </ul>
+
+
+        <div class="repos">
+            <a href="#" class="repo">Repos 1</a>
+            <a href="#" class="repo">Repos 2</a>
+            <a href="#" class="repo">Repos 3</a>
+        </div>
+
+    </div>
+</div>
+`
+main.innerHTML = cardHTML
+}
+
 form.addEventListener('submit',(e) => {
     e.preventDefault()
 
