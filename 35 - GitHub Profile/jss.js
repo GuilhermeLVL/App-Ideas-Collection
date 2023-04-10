1216b58bee06303d89923c29a0996f437c98ae93
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-10 15:50:59 -0300

    a

---
+++ 35 - GitHub Profile/jss.js
@@ -0,0 +1,10 @@
+const APIURL = 'https://api.github.com/users/'
+
+
+getUser('GuilhermeLVL')
+function getUser(username){
+    axios(APIURL + username)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+}
