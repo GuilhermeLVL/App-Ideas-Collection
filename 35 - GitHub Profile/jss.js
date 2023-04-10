d2aacaea265416608a6e63696fd241e81cdf9c04
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-10 17:11:05 -0300

    Update jss.js

--- 35 - GitHub Profile/jss.js
+++ 35 - GitHub Profile/jss.js
@@ -9,7 +9,7 @@ async function getUser(username){
     try{
 const { data } = await axios(APIURL + username)
    
-    console.log(data)
+    createUserCard(data)
     }catch(err){
         console.log(err)
 
@@ -18,31 +18,29 @@ const { data } = await axios(APIURL + username)
 
 }
 
-function createUserCard{
+function createUserCard(user){
 
     const cardHTML = ` 
     <div class="card">
     <div>
 
-        <img src="https://randomuser.me/api/portraits/men/30.jpg" alt="" class="avatar">
+        <img src="${user.avatar_url}" alt="" class="avatar">
 
     </div>
 
     <div class="user-info">
-        <h2>Johndoe</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, blanditiis?</p>
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
 
         <ul>
-            <li>300 <strong>Followers</strong></li>
-            <li>100 <strong>Following</strong></li>
-            <li>30 <strong>Repos</strong></li>
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
         </ul>
 
 
         <div class="repos">
-            <a href="#" class="repo">Repos 1</a>
-            <a href="#" class="repo">Repos 2</a>
-            <a href="#" class="repo">Repos 3</a>
+
         </div>
 
     </div>
